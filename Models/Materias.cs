8a7bb275bf1f563ba5a4abeba0d8bc1637ca2f76
SolumY0 <anaclaragouvea360@gmail.com>
2025-02-14 11:27:24 -0300

    Entrega da atividade

---
+++ Models/Materias.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGE.Models
+{
+    [Table("Materias")]
+    public class Materias
+    {
+        [Column("Id")]
+        [Display(Name = "Código da Matéria")]
+        public int Id { get; set; }
+
+        [Column("Titulo")]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [ForeignKey("ProfessoresId")]
+        [Display(Name = "Professores")]
+        public int ProfessoresId { get; set; }
+        public Professores? Professores { get; set; }
+
+    }
+}
---
+++ Models/Notas.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGE.Models
+{
+    [Table("Notas")]
+    public class Notas
+    {
+        [Column("Id")]
+        [Display(Name = "Código da Nota")]
+        public int Id { get; set; }
+
+        [ForeignKey("MateriasId")]
+        [Display(Name = "Materias")]
+        public int MateriasId { get; set; }
+        public Materias? Materias { get; set; }
+
+        [ForeignKey("AlunosId")]
+        [Display(Name = "Alunos")]
+        public int AlunosId { get; set; }
+        public Alunos? Alunos { get; set; }
+
+        [ForeignKey("EtapasId")]
+        [Display(Name = "Etapas")]
+        public int EtapasId { get; set; }
+        public Etapas? Etapas { get; set; }
+
+        [Column("Nota")]
+        [Display(Name = "Nota")]
+        public int Nota { get; set; }
+
+    }
+}
+
